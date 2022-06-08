23057bf4976d5d5e41fb02eb77edadf17868fce5
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2022-06-08 16:56:41 +0700

    add puma file

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :linked_files, %w{config/master.key}
 
+
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
